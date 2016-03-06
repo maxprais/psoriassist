30df50881791aad027519824e564f211b941e2b6
Max Prais <MaxPrais@gmail.com>
2016-03-06 16:23:55 +0200

    changed db to mysql

--- novartis/settings.py
+++ novartis/settings.py
@@ -9,8 +9,12 @@ DEBUG = True
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "dev.db",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "psoriassist",
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
