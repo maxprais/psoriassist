0dc863c91f137a2e8e27b147aa27f8c5a6b1676c
Max Prais <MaxPrais@gmail.com>
2016-03-14 14:51:25 +0200

    local storage

--- psoriassist/static/js/welcome.js
+++ psoriassist/static/js/welcome.js
@@ -4,16 +4,30 @@
 
 conversation = {};
 
+var savedConversation = ["WELCOME BACK, MICAH!","How are you doing today? Did you sleep well last night?"];
+savedConversation.push("test");
+
+if(window.localStorage['conversationText'] !== undefined){
+    //load conversation
+} else{
+    window.localStorage.setItem('conversationText', JSON.stringify(savedConversation));
+    console.log(JSON.parse(window.localStorage['conversationText']));
+}
+
+function save(sender, text){
+     window.localStorage.setItem('conversationText', JSON.stringify(sender, text));
+}
+
 $(function(conversation){
 
     conversation.init = $(function () {
         $('#second').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animatione', function () {
             var yesButton = $('<a></a>', {
                 id: 'initYes',
-                class: "btn btn-success waves-effect waves-light button",
+                class: "btn btn-success waves-effect waves-light button animated zoomIn",
                 text: 'Yes'
             });
-            var noButton = $('<a></a>', {id: 'initNo', class: "btn btn-warning waves-effect waves-light button", text: 'No'});
+            var noButton = $('<a></a>', {id: 'initNo', class: "btn btn-warning waves-effect waves-light animated zoomIn", text: 'No'});
             $('.screen-wrapper').append(yesButton, noButton);
 
             conversation.next();
@@ -27,7 +41,8 @@ $(function(conversation){
                 console.log($(this));
                 chosen.button($(this), $(this).next());
                 divs.create();
-                var head = $('<p></p>', {text: 'Great, glad to hear it', class: 'animated zoomIn'});
+                var head = $('<p></p>', {text: 'Great, glad to hear it. You know what they say:' +
+                'a good laugh and a long sleep are the best cures in the doctor\'s book', class: 'animated zoomIn'});
                 $('#2').append(head);
             })
         };
